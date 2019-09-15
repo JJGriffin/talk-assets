9a403151353a506968550e69235fda5028e4451b
Joe Griffin <joe@griffin.gb.net>
2019-09-15 12:58:17 +0100

    Created project for custom pricing sample.

---
+++ D365CECustomPricing/D365CE.CustomPricingTalk/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365CE.CustomPricingTalk
+{
+    public class Class1
+    {
+    }
+}
