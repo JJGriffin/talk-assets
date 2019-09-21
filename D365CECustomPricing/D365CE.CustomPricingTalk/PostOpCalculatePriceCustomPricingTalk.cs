d613aac58fbd0b41948a43fc5d5855ee9b03f914
Joe Griffin <joe@griffin.gb.net>
2019-09-21 16:06:27 +0100

    Further work building out the various sub-methods and general flow for calculating line item records.

--- D365CECustomPricing/D365CE.CustomPricingTalk/PostOpCalculatePriceCustomPricingTalk.cs
+++ D365CECustomPricing/D365CE.CustomPricingTalk/PostOpCalculatePriceCustomPricingTalk.cs
@@ -43,32 +43,30 @@ namespace D365CE.CustomPricingTalk
                     //Add shared variable, used earlier to check for infinite loops
                     context.SharedVariables.Add("CustomPrice", true);
                     context.ParentContext.SharedVariables.Add("CustomPrice", true);
+
                     //Get a reference to the organization service - used later
                     IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                     IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
-                    //TODO: Insert custom pricing logic here
-                    //As part of this, you can either: 
-                    //Apply the same custom pricing logic to all sales entities (as this example will do)
-                    //Use a switch statement to process custom logic for each entity individually e.g. for Opportunity:
-
-                    //switch (entity.LogicalName)
-                    //{
-                    //case "opportunity":
-                    //    DoSomethingHere();
-                    //    return;
-                    //}
-
-                    //List of available entities as part of this:
-                    //opportunity, opportunityproduct, quote, quotedetail, salesorder, salesorderdetail, invoice, invoicedetail
-
-                    //For further details, please refer to the following articles:
-                    //https://docs.microsoft.com/en-us/dynamics365/customer-engagement/developer/use-custom-pricing-products
-                    //https://docs.microsoft.com/en-us/dynamics365/customer-engagement/developer/sample-calculate-price-plugin
-
-                    
-
-
+                    switch(entity.LogicalName)
+                    {
+                        case "opportunityproduct":
+                        case "invoicedetail":
+                        case "quotedetail":
+                        case "salesorderdetail":
+                            tracing.Trace("Calculating " + entity.LogicalName + " as a line item calculation.");
+                            CalculateLineItem(entity, service, tracing);
+                            return;
+                        case "opportunity":
+                        case "quote":
+                        case "salesorder":
+                        case "invoice":
+                            tracing.Trace("Calculating " + entity.LogicalName + " as a sales document calculation.");
+                            CalculateSalesDocument(entity, service, tracing);
+                            return;
+                        default:
+                            throw new InvalidPluginExecutionException("Entity with name " + entity.LogicalName + " is not valid for calculating price information.");
+                    }
                 }
 
                 catch (FaultException<OrganizationServiceFault> ex)
@@ -103,9 +101,42 @@ namespace D365CE.CustomPricingTalk
             }
         }
 
+        private static void CalculateLineItem(EntityReference lineItem, IOrganizationService service, ITracingService tracing)
+        {
+            //Retrieve all required fields, including custom fields, from the Product record
+            //For this example, we return all fields; this is NOT recommended for Production environments
+            tracing.Trace("Retrieving " + lineItem.LogicalName + " record...");
+            Entity e1 = new Entity();
+            e1 = service.Retrieve(lineItem.LogicalName, lineItem.Id, new ColumnSet(true));
+            tracing.Trace("Retrieved " + lineItem.LogicalName + " record with ID " + e1.Id.ToString());
+
+            //If Order Line or Invoice Line, we need to check the related Order/Invoice to determine whether pricing is locked.
+            //If locked, then no calculation takes place, to prevent errors.
+
+            bool pricingLocked = CheckIfPricingLocked(e1, service, tracing);
+            if (pricingLocked == false)
+            {
+                //Check to see if the product is being sold below cost price - if so, throw an error
+
+                decimal cp;
+                decimal sp;
+
+                //Obtain the correct Discount and Freight Amounts for the product record
+
+                Money da = CalculateLineItemDiscount(e1, service, tracing);
+                Money fa = CalculateLineItemFreight(e1, service, tracing);
+
+            }
+
+        }
+        private static void CalculateSalesDocument(EntityReference salesDoc, IOrganizationService service, ITracingService tracing)
+        {
+
+        }
         private static Money CalculateLineItemDiscount(Entity lineItem, IOrganizationService service, ITracingService tracing)
         {
             tracing.Trace("Calculating discount for line item record...");
+
             //Initialise the values needed
             Money da = new Money(0);
             int? dp = lineItem.GetAttributeValue<int?>("jjg_discountpercent");
@@ -160,7 +191,55 @@ namespace D365CE.CustomPricingTalk
         }
         private static Money CalculateLineItemFreight(Entity lineItem, IOrganizationService service, ITracingService tracing)
         {
-            return new Money(0);
+            tracing.Trace("Calculating freight amount for line item record...");
+
+            //Initialise the values needed
+            //25 is the default amount we want to return if we cannot get the correct value
+            Money fa = new Money(25);
+
+            //Retrieve parent sales document record and its associated City value
+            //Use helper methods to retrieve the entity and lookup names we need and obtain ID for this record.
+            string peName = GetProductEntityName(lineItem.LogicalName);
+            string peIDName = GetProductEntityIDName(lineItem.LogicalName);
+            EntityReference peID = lineItem.GetAttributeValue<EntityReference>(peIDName);
+            Entity psd = new Entity();
+            psd = service.Retrieve(peName, peID.Id, new ColumnSet("shipto_city"));
+
+            //Get the city value - if NULL, then we default to Freight amount of £25
+
+            string city = psd.GetAttributeValue<string>("shipto_city0");
+
+            if (city != null)
+            {
+                //Query the Freight Amount entity, using the City name field.
+                //Can use QueryByAttribute here, due to simple nature of the query
+
+                QueryByAttribute qba = new QueryByAttribute("jjg_freightamount");
+                qba.ColumnSet = new ColumnSet("jjg_freightamountvalue");
+                qba.Attributes.AddRange("jjg_city");
+                qba.Values.AddRange(city);
+
+                EntityCollection faEntity = service.RetrieveMultiple(qba);
+
+                //Get the Freight Amount value
+                //Always grab the top record only - the instance in question should have duplicate detection rules in place, to prevent potential conflicts
+
+                if (faEntity.TotalRecordCount == 1)
+                {
+                    fa = faEntity[0].GetAttributeValue<Money>("jjg_freightamountvalue");
+                }
+
+                else
+                {
+                    tracing.Trace("Could not obtain a Freight Amount Value, defaulting to £25");
+                }
+            }
+            else
+            {
+                tracing.Trace("No Ship To City value supplied for " + psd.LogicalName + " record, defaulting to £25");
+            }
+
+            return fa;
         }
         private static string GetProductEntityName(string entity)
         {
@@ -231,6 +310,49 @@ namespace D365CE.CustomPricingTalk
                     pe = "";
                     return pe;
             }
+        }
+        private static bool CheckIfPricingLocked(Entity entity, IOrganizationService service, ITracingService tracing)
+        {
+            tracing.Trace("Determining whether prices are locked or not...");
+            bool pricingLocked = false;
+            if (entity.LogicalName == "salesorderdetail" || entity.LogicalName == "invoicedetail")
+            {
+                tracing.Trace(string.Concat("Entity is ", entity.LogicalName, ", checking whether prices are locked..."));
+
+                if (entity.LogicalName == "salesorderdetail")
+                {
+                    Entity e2 = new Entity();
+                    e2 = service.Retrieve("salesorder", entity.GetAttributeValue<EntityReference>("salesorderid").Id, new ColumnSet("ispricelocked"));
+                    if (e2.GetAttributeValue<Boolean>("ispricelocked") == true)
+                    {
+                        tracing.Trace(string.Concat("salesorder record with ID ", e2.Id.ToString(), " has its pricing locked."));
+                        pricingLocked = true;
+                    }
+                    else
+                    {
+                        tracing.Trace(string.Concat("salesorder record with ID ", e2.Id.ToString(), " does NOT have its pricing locked."));
+                    }
+                }
+                else if (entity.LogicalName == "invoicedetail")
+                {
+                    Entity e2 = new Entity();
+                    e2 = service.Retrieve("invoice", entity.GetAttributeValue<EntityReference>("invoiceid").Id, new ColumnSet("ispricelocked"));
+                    if (e2.GetAttributeValue<Boolean>("ispricelocked") == true)
+                    {
+                        tracing.Trace(string.Concat("invoice record with ID ", e2.Id.ToString(), " has its pricing locked."));
+                        pricingLocked = true;
+                    }
+                    else
+                    {
+                        tracing.Trace(string.Concat("invoice record with ID ", e2.Id.ToString(), " does NOT have its pricing locked."));
+                    }
+                }
+            }
+            else
+            {
+                tracing.Trace("Entity is opportunityproduct or quotedetail, pricing will not be locked.");
+            }
+            return pricingLocked;
         }
     }
 }
